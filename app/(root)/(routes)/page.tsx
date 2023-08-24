c2e5578e09fcbecad563e0b8ed7374a4e09d68c9
Zaid-maker <pzhafeez@gmail.com>
2023-08-24 21:11:14 +0500

    update routes

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const RootPage = async () => {
+  return <h1>Main Page</h1>;
+};
+
+export default RootPage;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Companion AI",
+  description: "You customized companion AI platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
+        {/*<Sidebar />*/}
+      </div>
+      <main className="md:pl-20 pt-16 h-full">{children}</main>
+    </div>
+  );
+}
