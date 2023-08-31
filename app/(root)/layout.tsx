da799421b161ab715923679d16b5315273277337
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-31 15:19:16 +0000

    add `Sidebar` component and cleanup root app layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,5 @@
+import { Sidebar } from "@/components/sidebar";
 import { Navbar } from "@/components/navbar";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-import "../globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Companion AI",
-  description: "You customized companion AI platform",
-};
 
 export default function RootLayout({
   children,
@@ -20,7 +10,7 @@ export default function RootLayout({
     <div className="h-full">
       <Navbar />
       <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
-        {/*<Sidebar />*/}
+        <Sidebar />
       </div>
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
