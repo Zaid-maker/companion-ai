2d6866a8f68d555d40b05d357cdf1ff5cf7e44d1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-02 16:55:10 +0000

    fetch categories from prismadb

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,6 +1,9 @@
 import SearchInput from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
 
 const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
