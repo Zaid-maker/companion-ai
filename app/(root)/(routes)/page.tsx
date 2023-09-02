6878533a095930993d8b55e3a19f838b22e53f1c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-02 17:00:29 +0000

    pass in the data prop in root aswell

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -8,7 +8,7 @@ const RootPage = async () => {
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
-      <Categories />
+      <Categories data={categories} />
     </div>
   );
 };
