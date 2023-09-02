2d2d4879bdcae1f9bf981c3d1fedf0ae30525747
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-02 16:57:55 +0000

    add categories component

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import SearchInput from "@/components/SearchInput";
+import { Categories } from "@/components/categories";
 import prismadb from "@/lib/prismadb";
 
 const RootPage = async () => {
@@ -7,6 +8,7 @@ const RootPage = async () => {
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories />
     </div>
   );
 };
