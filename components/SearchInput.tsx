74cd8e511d136410acef6788d0e2f87cf69c4d56
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-02 10:21:36 +0000

    add onChange function for Input to work

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { useDebounce } from "@/hooks/use-debounce";
 import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEventHandler, useState } from "react";
 
 const SearchInput = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValues] = useState(name || "");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValues(e.target.value);
+  };
+
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-      <Input placeholder="Search..." className="pl-10 bg-primary/10" />
+      <Input
+        onChange={onChange}
+        value={value}
+        placeholder="Search..."
+        className="pl-10 bg-primary/10"
+      />
     </div>
   );
 };
