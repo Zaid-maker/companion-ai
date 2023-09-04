3643c2ba908ef07d22c81dc55000dd1e86cd2cea
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-04 10:34:25 +0000

    add `categoryId` for categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { Category } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface CategoriesProps {
@@ -9,11 +10,17 @@ interface CategoriesProps {
 }
 
 export const Categories = ({ data }: CategoriesProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+
   return (
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <button
         className={cn(
-          "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition"
+          "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition",
+          !categoryId ? "bg-primary/25" : "bg-primary/10"
         )}
       >
         Newest
@@ -21,7 +28,8 @@ export const Categories = ({ data }: CategoriesProps) => {
       {data.map((item) => (
         <button
           className={cn(
-            "flex items-center text-center text-xs md:text-sm px-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition"
+            "flex items-center text-center text-xs md:text-sm px-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition",
+            item.id === categoryId ? "bg-primary/25" : "bg-primary/10"
           )}
           key={item.id}
         >
