9f71f55d33eb32128b10c3328e466228917141b9
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-04 15:55:24 +0000

    add `formSchema` for companion

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 
 interface CompanionFormProps {
   initialData: Companion | null;
   categories: Category[];
 }
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required for the companion",
+  }),
+  description: z.string().min(1, {
+    message: "Description is required for the companion",
+  }),
+  instructions: z.string().min(200, {
+    message:
+      "Instructions is required for the companion, atleast 200 characters",
+  }),
+  seed: z.string().min(200, {
+    message: "Seed is required for the companion, atleast 200 characters",
+  }),
+  src: z.string().min(1, {
+    message: "Image is required for the companion",
+  }),
+  categoryId: z.string().min(200, {
+    message: "Category is required for the companion",
+  }),
+});
 
 export const CompanionForm = ({
   initialData,
