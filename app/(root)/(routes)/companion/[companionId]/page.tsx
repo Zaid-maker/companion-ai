1df8ef198e43156bb37022b68d13ded784f4b639
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-04 11:33:47 +0000

    add companion and categories method to fetch data from prismadb

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,11 +1,21 @@
+import prismadb from "@/lib/prismadb";
+
 interface CompanionPageProps {
   params: {
     companionId: string;
   };
 }
 
-const CompanionPage = () => {
-  return <div>CompanionPage</div>;
+const CompanionPage = async ({ params }: CompanionPageProps) => {
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return <CompanionForm initialDara={companion} categories={categories} />;
 };
 
 export default CompanionPage;
