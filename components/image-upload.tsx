de5cd6466e4cd8048a256fc20cf9a121096ceb47
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-06 08:39:18 +0000

    add some props

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -27,7 +27,11 @@ export const ImageUpload = ({
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
-      <CldUploadButton>
+      <CldUploadButton
+        options={{ maxFiles: 1 }}
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        uploadPreset="t4drjppf"
+      >
         <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
           <div className="relative h-40 w-40">
             <Image
