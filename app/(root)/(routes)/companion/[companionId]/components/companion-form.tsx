1f106b32974e8948398d5bf4b15d74314f87a355
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-06 11:17:21 +0000

    add toast noti on onSubmit function

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -27,6 +27,8 @@ import { ImageUpload } from "@/components/image-upload";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -75,6 +77,9 @@ export const CompanionForm = ({
   initialData,
   categories,
 }: CompanionFormProps) => {
+  const { toast } = useToast();
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
@@ -96,7 +101,21 @@ export const CompanionForm = ({
       } else {
         await axios.post("/api/companion", values);
       }
-    } catch (error) {}
+
+      toast({
+        description: "Success",
+        duration: 3000,
+      });
+
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      toast({
+        variant: "destructuve",
+        description: "Something went wrong",
+        duration: 3000,
+      });
+    }
   };
 
   return (
